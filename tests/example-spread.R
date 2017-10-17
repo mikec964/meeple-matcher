fc04962ed789c2fc05ca377a65aa16a14bd0501c
Mike Combs <mike@combsnet.com>
2017-10-17 02:45:20 -0400

    Added typical spread() with rows spread into columns.

--- tests/example-spread.R
+++ tests/example-spread.R
@@ -3,22 +3,33 @@ library(tidyr)
 
 #rm(list = ls())
 
-df1 <- data.frame(id = LETTERS[c(1:3,1:3,1)],
+game = c("Catan", "Carcassonne", "Pandemic", "Dominion",
+         "7 Wonders", "Ticket to Ride", "Agricola",
+         "Puerto Rico", "Small World", "Power Grid")
+
+df1 <- data.frame(id = sample(game, 7),
                  attr = c("h","l","h","r","r","b","extra"))
 
-df2 <- data.frame(id = LETTERS[c(1:3,1:3,1)],
+df2 <- data.frame(id = sample(game, 7),
                  theme = c("sci","farm","racing","space","fan","card","cats"))
-
 
 #attribute binaries
 df1$TF <- 1
-df1 <- df1 %>% spread(attr, TF, fill = 0)
-names(df1)[-1] <- paste0("attr_",names(df1)[-1])
+df1.wide <- df1 %>% spread(attr, TF, fill = 0)
+names(df1.wide)[-1] <- paste0("attr_",names(df1.wide)[-1])
 
 #theme binaries
 df2$TF <- 1
-df2 <- spread(df2, theme, TF, fill = 0)
-names(df2)[-1] <- paste0("theme_", names(df2)[-1])
+df2.wide <- spread(df2, theme, TF, fill = 0)
+names(df2.wide)[-1] <- paste0("theme_", names(df2.wide)[-1])
 
-#Join
-df3 <- merge(df1, df2, by = "id")
+#Join, if they have ID in common
+df3 <- merge(df1.wide, df2, by = "id")
+
+# Spread game ratings into a rows per game, cols per gamer
+# The spread function works if col1/game.id is unique?
+gamers = c("jdoe", "gamerDude", "c3po")
+df4 <- data.frame(game = c(game[1:3], game[1:3]),
+                      gamer = c(gamers[1:3], gamers[2:3], gamers[1]),
+                      rating = c(1:6))
+df4.wide <- spread(df4, gamer, rating)

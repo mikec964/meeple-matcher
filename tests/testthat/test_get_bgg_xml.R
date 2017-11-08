e5fc2e79439240718bd2b02f3013578647408668
Mike Combs <mike@combsnet.com>
2017-11-08 14:33:09 -0500

    Updated cache tests to match new cache path structure, corrected logical operators.

--- tests/testthat/test_get_bgg_xml.R
+++ tests/testthat/test_get_bgg_xml.R
@@ -1,47 +1,44 @@
 library('testthat')
 
 context("GetBGGXML")
+# Uses GetGamerCollection and GetGameRatings
 
 test_that("GetBGGXML loads collection XML from server", {
-  # Do we build a correct URL for collections?
-
-  test.cache <- paste0("../../", kCacheName) # used when run from tests dir
-
   customer <- "mikec"
-  res1 <- evaluate_promise(GetGamerCollection(customer, "",
-                           use.cache=FALSE, make.cache=FALSE))
-  expect_equal(res1$messages[1],
-               "Getting: https://boardgamegeek.com/xmlapi2/collection?username=mikec&subtype=boardgame&stats=1&brief=1\n")
-
-  # Create a cache file for mikec
+  cache.path <- paste0(kCacheName, "collection/username=", customer, ".xml")
+  tCache.path <- paste0("../../", cache.path) # used when run from tests dir
   tUrl <- "https://boardgamegeek.com/xmlapi2/collection?username=mikec&subtype=boardgame&stats=1&brief=1"
-  res3 <- GetBGGXML(tUrl, "", use.cache=FALSE, make.cache=TRUE)
-  expect_true(file.exists(paste0(kCacheName, "collection-username=mikec.xml"))
-              || file.exists(paste0(test.cache, "collection-username=mikec.xml")))
+
+  # Do we build a correct URL for collections?
+  res1 <- evaluate_promise(
+    GetGamerCollection(customer, use.cache=FALSE, make.cache=FALSE))
+  expect_equal(res1$messages[1], paste0("Getting: ", tUrl, "\n"))
+
+  # Can we build a cache file?
+  res3 <- GetBGGXML(tUrl, use.cache=FALSE, make.cache=TRUE)
+  expect_true(file.exists(cache.path) | file.exists(tCache.path))
   #print(getwd())
   #print(test.cache)
 })
 
 test_that("GetBGGXML loads game ratings XML from server", {
+  game.id <- 40
+  cache.path <- paste0(kCacheName, "thing/id=", game.id,
+                       "/id=", game.id, "-2", ".xml")
+  tCache.path <- paste0("../../", cache.path) # used when run from tests dir
+  tUrl <- "https://boardgamegeek.com/xmlapi2/thing?id=40&ratingcomments=1&page=1"
+
   # Do we build a correct URL for ratings?
+  res2 <- evaluate_promise(
+    GetGameRatings(game.id, use.cache=FALSE, make.cache=FALSE))
+  expect_equal(res2$messages[1], paste0("Getting: ", tUrl, "\n"))
 
-  test.cache <- paste0("../../", kCacheName) # used when run from tests dir
-
-  game.id <- 38453
-  res2 <- evaluate_promise(GetGameRatings(game.id, "",
-                                          use.cache=FALSE, make.cache=FALSE))
-  expect_equal(res2$messages[1],
-               "Getting: https://boardgamegeek.com/xmlapi2/thing?id=38453&ratingcomments=1&page=1\n")
-
-  # Create a cache file for game 38453
-  tUrl <- "https://boardgamegeek.com/xmlapi2/thing?id=38453&ratingcomments=1&page=1"
+  # Can we build a page 1 cache file?
   res.game.pg1 <- GetBGGXML(tUrl, use.cache=FALSE, make.cache=TRUE)
-  expect_true(file.exists(paste0(kCacheName, "thing-id=38453/thing-id=38453-1.xml"))
-              || file.exists(paste0(test.cache, "thing-id=38453/thing-id=38453-1.xml")))
+  expect_true(file.exists(cache.path) | file.exists(tCache.path))
 
-  # Create a page-2 cache file for game 38453
-  tUrl <- "https://boardgamegeek.com/xmlapi2/thing?id=38453&ratingcomments=1&page=2"
+  # Create a page-2 cache file for game 40
+  tUrl <- "https://boardgamegeek.com/xmlapi2/thing?id=40&ratingcomments=1&page=2"
   res.game.pg2 <- GetBGGXML(tUrl, use.cache=FALSE, make.cache=TRUE)
-  expect_true(file.exists(paste0(kCacheName, "thing-id=38453/thing-id=38453-2.xml"))
-              || file.exists(paste0(test.cache, "thing-id=38453/thing-id=38453-2.xml")))
+  expect_true(file.exists(cache.path) | file.exists(tCache.path))
 })

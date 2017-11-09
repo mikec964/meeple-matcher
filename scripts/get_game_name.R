568cb87014aeda506443e515dd9fa42e65508dbf
Mike Combs <mike@combsnet.com>
2017-11-08 23:51:09 -0500

    Added function to get attribute value (given key) from tall table of attributes

--- scripts/get_game_name.R
+++ scripts/get_game_name.R
@@ -4,3 +4,10 @@ GetGameName <- function(gid) {
   games <- collection.selected[collection.selected$game.id == gid, ]
   return(games$game[1])
 }
+
+GetGameAttr <- function(game.attrs, attr) {
+  # returns vector of values given attribute key
+
+  value <- game.attrs[game.attrs$key == attr, "value"]
+  return(value)
+}

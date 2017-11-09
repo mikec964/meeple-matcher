a8cd3317336aa70567237e026722bd3e610b0427
Mike Combs <mike@combsnet.com>
2017-11-08 23:54:55 -0500

    Changed: Now accepts a list of game ids. Now loads ALL ratings for specified games. No longer accepts specific page to load as a param.

--- scripts/get_game_ratings.R
+++ scripts/get_game_ratings.R
@@ -1,26 +1,55 @@
 library(dplyr)
+library(futile.logger)
 library(XML)
 
-GetGameRatings <- function(game.id1, test.file="", page=1,
-                           use.cache=TRUE, make.cache=TRUE) {
+GetGameRatings <- function(game.ids, use.cache=TRUE, make.cache=TRUE) {
   # Get ratings and gamers who have rated
-  #
+
   # space_alert.df <- GetGameRatings(38453)
-  # mc.df <- GetGameRatings(38453, "data/thing-id=38453.xml")
+  # mc.df <- GetGameRatings(38453, use.cache=TRUE)
   #
   # | gameid | game         | gamername | rating |
   # |--------|--------------|-----------|--------|
   # | 38453  | Space Alert  | mikec     | 6      |
   # | 38453  | Space Alert  | gamerdude | 7      |
 
+  flog.debug("GetGameRatings: %s, %s, %s", game.ids, use.cache, make.cache)
+  for(g in 1:length(game.ids)) {
+    game.id <- game.ids[g]
+    tGame.attrs <- GetGameAttrs(game.id)
+    num.comments <- as.integer(tGame.attrs[tGame.attrs$key=="comments",]$value)
+    num.pages <- as.integer(((num.comments - 1) / 100) + 1)
+    game <- GetGameAttr(tGame.attrs, "game")[1]
+    print(sprintf("%s of %s, loading %s comments for %s, %s",
+                  g, length(game.ids), num.comments,
+                  game.id, game))
+    for(p in 1:num.pages) {
+      if(p == 1) {
+        ratings.tbl <- .GetRatingPage(game.id, page=p)
+      } else {
+        ratings.tbl <- bind_rows(ratings.tbl, .GetRatingPage(game.id, page=p))
+      }
+    }
+    if(g == 1) {
+      big.ratings <- ratings.tbl
+    } else {
+      big.ratings <- bind_rows(big.ratings, ratings.tbl)
+    }
+  }
+  return(big.ratings)
+}
+
+
+
+#------------------------------------
+.GetRatingPage <- function(game.id, page=1, use.cache=TRUE, make.cache=TRUE) {
   root.path <- "https://boardgamegeek.com/xmlapi2/"
   collection.params <- paste0("thing?",
-                              "id=", game.id1,
+                              "id=", game.id,
                               "&ratingcomments=1",
                               "&page=", page)
   collection.path <- paste0(root.path, collection.params)
-  collection.root <- GetBGGXML(collection.path, test.file,
-                               use.cache, make.cache)
+  collection.root <- GetBGGXML(collection.path, use.cache, make.cache)
 
   # Move into dataframe (from doc)
   comment.list <- xpathSApply(collection.root, '//*/item/comments/comment', xmlAttrs)
@@ -37,16 +66,6 @@ GetGameRatings <- function(game.id1, test.file="", page=1,
     game.id <- comment.list
     rating <- comment.list
   }
-
-#  type <- (unlist(item.attr["type"]))
-#  description <- xpathSApply(collection.root, '//*/item/description', xmlValue)
-#  published <- as.integer(xpathSApply(collection.root, '//*/item/yearpublished', xmlAttrs))
-#  comments.attr <- unlist(xpathApply(collection.root, '//*/item/comments', xmlAttrs)) # page, totalitems
-#  page <- as.integer(comments.attr["page"])
-#  number.comments <- as.integer(comments.attr["totalitems"])
-
-
   ratings.tbl <- tibble(gamer, game, game.id, rating)
   return(ratings.tbl)
 }
-
--- tests/testthat/test_get_game_ratings.R
+++ tests/testthat/test_get_game_ratings.R
@@ -1,32 +1,42 @@
+library('testthat')
+
+context("GetGameRatings")
+
 test_that("GetGameRatings loads XML from server", {
-  # Are we generating a good URL?
-  game.id <- 38453
-  res2 <- evaluate_promise(GetGameRatings(game.id, "",
-                                          use.cache=FALSE, make.cache=FALSE))
-  expect_equal(res2$messages[1],
-               "Getting: https://boardgamegeek.com/xmlapi2/thing?id=38453&ratingcomments=1&page=1\n")
+  game.id <- 40
+  tUrl1 <- "https://boardgamegeek.com/xmlapi2/thing?id=40&ratingcomments=1&page=1"
+  tUrl2 <- "https://boardgamegeek.com/xmlapi2/thing?id=40&ratingcomments=1&page=2"
 
-  # Test page 2 URL
-  res2 <- evaluate_promise(GetGameRatings(game.id, "", page=2,
-                                          use.cache=FALSE, make.cache=FALSE))
-  expect_equal(res2$messages[1],
-               "Getting: https://boardgamegeek.com/xmlapi2/thing?id=38453&ratingcomments=1&page=2\n")
+  # Are we generating a good page 1 URL?
+  res2 <- evaluate_promise(
+    .GetRatingPage(game.id, use.cache=FALSE, make.cache=TRUE))
+  expect_equal(res2$messages[1], paste0("Getting: ", tUrl1, "\n"))
 
+  # Are we generating a good page 2 URL?
+  res2 <- evaluate_promise(
+    .GetRatingPage(game.id, page=2, use.cache=FALSE, make.cache=TRUE))
+  expect_equal(res2$messages[1], paste0("Getting: ", tUrl2, "\n"))
 })
 
 test_that("GetGameRatings parses XML", {
   # This tests from a file so we can check parsing
-  game.id <- 38453
-  game.file <- "../data/thing-id=38453.xml"
+  game.id <- 40
 
-  res2 <- evaluate_promise(GetGameRatings(game.id, game.file))
+  # Are there results from page 1?
+  res2 <- evaluate_promise(GetGameRatings(game.id, use.cache=TRUE))
   ratings.tbl <- res2$result
-
   # Check the column types
   #   gamer, game, game.id, rating
   item <- 1
   expect_true(is.character(ratings.tbl[[item, "gamer"]]))
   expect_true(is.character(ratings.tbl[[item, "game"]]))
   expect_true(is.integer(ratings.tbl[[item, "game.id"]]))
+  expect_true(is.integer(ratings.tbl[[item, "rating"]]))
+
+  # Are there results from page 2?
+  res2 <- evaluate_promise(.GetRatingPage(game.id, page=2, use.cache=TRUE))
+  ratings.tbl <- res2$result
+  item <- 1
+  expect_true(is.character(ratings.tbl[[item, "gamer"]]))
   expect_true(is.integer(ratings.tbl[[item, "rating"]]))
 })

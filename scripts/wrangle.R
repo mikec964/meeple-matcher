ebf718adfdc0774fd3dacbf40c4fd15fa414914d
Mike Combs <mike@combsnet.com>
2017-11-15 18:23:05 -0500

    Updated comments, this is mainly a data loader now.

--- scripts/wrangle.R
+++ scripts/wrangle.R
@@ -25,97 +25,115 @@ library(tidyr)
 # The same game may have names in different languages or editions
 # 'games' columns are per game, like "description", categories and mechanics.
 
-customer <- "mikec"
-collection.customer <- read_tsv("tables/collection-customer.tsv")
-games.ratings       <- read_tsv("tables/games-ratings.tsv")
-games.ratings       <- games.ratings[, c("game.id", "game", "gamer", "rating")]
-games.attrs         <- read_tsv("tables/games-attrs.tsv")
-collection.selected <- read_tsv("tables/collection-selected.tsv")
-collection.selected <- bind_rows(collection.customer, collection.selected)
-gamers.adjacent     <- unique(games.ratings$gamer)
-gamers.selected     <- read_lines("tables/gamers-selected.tsv")
+# customer <- "mikec"
+# collection.customer <- read_tsv("tables/collection-customer.tsv")
+# games.ratings       <- read_tsv("tables/games-ratings.tsv")
+# games.ratings       <- games.ratings[, c("game.id", "game", "gamer", "rating")]
+# games.attrs         <- read_tsv("tables/games-attrs.tsv")
+# collection.selected <- read_tsv("tables/collection-selected.tsv")
+# collection.selected <- bind_rows(collection.customer, collection.selected)
+# gamers.adjacent     <- unique(games.ratings$gamer)
+# gamers.selected     <- read_lines("tables/gamers-selected.tsv")
+# games.categories <- unique(
+#   games.attrs[games.attrs$key == attrs.tags["category"],]$value)
+# games.mechanics <- unique(
+#   games.attrs[games.attrs$key == attrs.tags["mechanic"],]$value)
 
 
-# Build wide table (games.details) of game attributes
-#---------------------------------
-# Per row: Games that have been rated
-# Cols for attributes like yearpublished and maxplayers
-# Cols for multiple value tags like boardgamecategory and boardgamemechanic
 
-games.details <- collection.customer[!is.na(collection.customer$rating),]
+#--------------------------------------
+#--------------------------------------
+WidenAttrs <- function(games.attrs) {
+  # Build wide table (games.details) of game attributes
+  #---------------------------------
+  # Per row: Games that have been rated
+  # Cols for attributes like yearpublished and maxplayers
+  # Cols for multiple value tags like boardgamecategory and boardgamemechanic
 
-# These attributes have a single value; each attribute can be a column heading
-attrs.unique <- c("yearpublished", "minplayers", "maxplayers",
-                  "playingtime", "minplaytime", "maxplaytime",
-                  "description", "comments")
-for(a in attrs.unique) {
-  v <- games.attrs[games.attrs$key == a, c("game.id", "value")]
-  names(v)[-1] <- a
-  games.details <- left_join(games.details, v, by="game.id")
+  # These attributes have a single value; each attribute can be a column heading
+  games.details <- collection.customer[!is.na(collection.customer$rating),]
+  attrs.unique <- c("yearpublished", "minplayers", "maxplayers",
+                    "playingtime", "minplaytime", "maxplaytime",
+                    "description", "comments")
+  for(a in attrs.unique) {
+    v <- games.attrs[games.attrs$key == a, c("game.id", "value")]
+    names(v)[-1] <- a
+    games.details <- left_join(games.details, v, by="game.id")
+  }
+  return(games.details)
 }
 
-# These attributes have N values; each value can be a column heading
-attrs.tags <- c("boardgamecategory", "boardgamemechanic")
-names(attrs.tags) <- c("category", "mechanic")
-for(a in attrs.tags) {
-  v <- games.attrs[games.attrs$key == a, c("game.id", "value")]
-  v$TF <- 1
-  v <- v %>% spread(value, TF, fill = 0)
-  names(v)[-1] <- paste0(names(a), ".", names(v)[-1])
-  games.details <- left_join(games.details, v, by="game.id")
+
+
+#--------------------------------------
+#--------------------------------------
+WidenTags <- function(games.attrs) {
+  # These attributes have N values; each value can be a column heading
+  attrs.tags <- c("boardgamecategory", "boardgamemechanic")
+  names(attrs.tags) <- c("category", "mechanic")
+  for(a in attrs.tags) {
+    v <- games.attrs[games.attrs$key == a, c("game.id", "value")]
+    v$TF <- 1
+    v <- v %>% spread(value, TF, fill = 0)
+    names(v)[-1] <- paste0(names(a), ".", names(v)[-1])
+    games.details <- left_join(games.details, v, by="game.id")
+  }
+  return(games.details)
 }
 
 
-# Get unique categories and mechanics (from customer's games)
-#---------------------------------
-games.categories <- unique(
-  games.attrs[games.attrs$key == attrs.tags["category"],]$value)
-games.mechanics <- unique(
-  games.attrs[games.attrs$key == attrs.tags["mechanic"],]$value)
 
-
-# Build grids of games, gamers, and ratings
+# Build wide tables of games, gamers, and ratings
 #---------------------------------
 # Rows = games, Cols = gamers, value = rating
 
-# customer.grid: rows = 160 customer's games, cols = 158,706 gamers
-games.ratings <- distinct(games.ratings, game.id, gamer, .keep_all = TRUE)
-customer.grid <- spread(games.ratings, gamer, rating)
+CustomerGrid <- function(games.ratings) {
+  # customer.grid: rows = 160 customer's games, cols = 158,706 gamers
+  games.ratings <- distinct(games.ratings, game.id, gamer, .keep_all = TRUE)
+  customer.grid <- spread(games.ratings, gamer, rating)
+}
 
-# collection.grid: rows = 16,685 games, cols = 502 adjacent gamers
-collection.ratings <- collection.selected[, c("game.id", "game", "gamer", "rating")]
-collection.ratings <- distinct(collection.ratings, game.id, gamer, .keep_all = TRUE)
-collection.grid <- spread(collection.ratings, gamer, rating)
+CollectionGrid <- function(collection.selected) {
+  # collection.grid: rows = 16,685 games, cols = 502 adjacent gamers
+  collection.ratings <- collection.selected[, c("game.id", "game", "gamer", "rating")]
+  collection.ratings <- distinct(collection.ratings, game.id, gamer, .keep_all = TRUE)
+  collection.grid <- spread(collection.ratings, gamer, rating)
+}
 
-# most.grid: rows = 160 games, cols = 252 of "most" adjacent gamers
-## NOTE: This is 224 rows instead of 160 because some games have dupe names.
-## TODO: Remove dupe names.
+MostGrid <- function(collection.selected) {
+  # most.grid: rows = 160 games, cols = 252 of "most" adjacent gamers
+  ## NOTE: This is 224 rows instead of 160 because some games have dupe names.
+  ## TODO: Remove dupe names.
 
-# remove games with no ratings
-collection.rated <- collection.selected[
-  !is.na(collection.selected$rating),]
+  # remove games with no ratings
+  #collection.rated <- collection.selected[
+  #  !is.na(collection.selected$rating),]
+  collection.rated <- collection.selected
 
-# gamers.most = table of gamers and # of games rated
-# only gamers who have rated 2nd to 4th quartile # of games
-gamers.most <- collection.rated %>%
-  group_by(gamer) %>%
-  count %>%
-  arrange(n)
-names(gamers.most)[-1] <- c("ratings")
-gamers.most <- gamers.most[(gamers.most$ratings >=
-                                 fivenum(gamers.most$ratings)[2]),]
+  # gamers.most = table of gamers and # of games rated
+  # only gamers who have rated 2nd to 4th quartile # of games
+  gamers.most <- collection.rated %>%
+    group_by(gamer) %>%
+    count %>%
+    arrange(n)
+  names(gamers.most)[-1] <- c("ratings")
+  #gamers.most <- gamers.most[(gamers.most$ratings >=
+  #                                 fivenum(gamers.most$ratings)[2]),]
+  gamers.most <- gamers.most[(gamers.most$ratings >= 5),]
 
-# most.grid = games rated by most gamers
-most.ratings <- collection.rated %>%
-  semi_join(gamers.most, by = "gamer") %>%
-  semi_join(collection.customer, by = "game.id") %>%
-  distinct(game.id, gamer, .keep_all = TRUE) %>%
-  select(game.id, game, gamer, rating)
-most.grid <- spread(most.ratings, gamer, rating)
-# remove games with dupe names
-# - goes from 268 to 147 games
-# - BUG: throws out ratings of games with alt names
-most.grid <- distinct(most.grid, game.id, .keep_all=TRUE)
+  # most.grid = games rated by most gamers
+  most.ratings <- collection.rated %>%
+    semi_join(gamers.most, by = "gamer") %>%
+    semi_join(collection.customer, by = "game.id") %>%
+    distinct(game.id, gamer, .keep_all = TRUE) %>%
+    select(game.id, game, gamer, rating)
+  most.grid <- spread(most.ratings, gamer, rating)
+  # remove games with dupe names
+  # - goes from 268 to 147 games
+  # - BUG: throws out ratings of games with alt names
+  most.grid <- distinct(most.grid, game.id, .keep_all=TRUE)
+}
+
 
 
 # Add the customer ratings as a column to any grid:

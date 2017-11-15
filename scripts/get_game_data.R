daa9d47c2cde30ea05aa6655147916437de5784a
Mike Combs <mike@combsnet.com>
2017-11-15 17:50:28 -0500

    Updated to pass list of game.ids instead of individual game.id param.

--- scripts/get_game_data.R
+++ scripts/get_game_data.R
@@ -4,22 +4,12 @@ library(readr)
 GetGameData <- function(game.ids) {
   # For a list of games, get attributes and ratings from XML
   # Return/set global vars: games.attrs, games.ratings
-  # Store results as TSV: games-attrs.tsv, games-ratings.tsv
   #
   # Loads (slowly) in series, should be in parallel
 
   game.ids <- sort(game.ids)
-  for(g in 1:length(game.ids)) {
-    game.id <- game.ids[g]
-
-    attrs.tbl <- GetGameAttrs(game.ids)
-    games.attrs <<- attrs.tbl # create global var
-    write_tsv(attrs.tbl, "tables/games-attrs.tsv",
-              na = "NA", append = FALSE, col_names = TRUE)
-
-    ratings.tbl <- GetGameRatings(game.ids)
-    games.ratings <<- ratings.tbl # create global var
-    write_tsv(ratings.tbl, "tables/games-ratings.tsv",
-              na = "NA", append = FALSE, col_names = TRUE)
-  }
+  attrs.tbl <- GetGameAttrs(game.ids)
+  games.attrs <<- attrs.tbl # create global var
+  ratings.tbl <- GetGameRatings(game.ids)
+  games.ratings <<- ratings.tbl # create global var
 }

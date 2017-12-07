928672864876a92ee7f2709edf5fc7df70ea5c01
Mike Combs <mike@combsnet.com>
2017-12-06 19:50:42 -0500

    Change (rotate) to row per gamer. Add row and col names, return as a matrix (not a tbl).

--- scripts/make_rating_matrix.R
+++ scripts/make_rating_matrix.R
@@ -3,7 +3,7 @@ library(tidyr)
 
 MakeRatingMatrix <- function(collection, min.game.rates = 1,
                              min.gamer.rates = 5) {
-  # Return matrix of ratings with row per game, col per gamer
+  # Return matrix of ratings with row per gamer, col per game
 
   # collection: wide df with gamer, game.id, rating columns
   # min.game.rates: remove games with fewer ratings
@@ -14,18 +14,19 @@ MakeRatingMatrix <- function(collection, min.game.rates = 1,
   # also remove unneeded columns
   ratings.tall <- collection.unique[, c("gamer", "game.id", "rating")]
   # spread!
-  ratings.wide <- spread(ratings.tall, gamer, rating)
+  ratings.wide <- spread(ratings.tall, game.id, rating) # rowname <- gamer
+  ratings.matrix <- as(ratings.wide[-1], "matrix")
+  rownames(ratings.matrix) <- ratings.wide$gamer
+  colnames(ratings.matrix) <- colnames(ratings.wide[,-1])
 
-  # remove games (obs/cases/rows) with fewer than <minRates> ratings
-  rate.rows <- !is.na(ratings.wide)[, -1]
+  # remove gamers (obs/cases/rows) with fewer than <minRates> ratings
+  rate.rows <- !is.na(ratings.matrix)[,]
   c.rate.rows <- rowSums(rate.rows)
-  ratings.wide2 <- ratings.wide[c.rate.rows >= min.game.rates, ]
-  # remove gamers (vars/cols) with fewer than <minRates> games rated
-  rate.cols <- !is.na(ratings.wide2)[, -1]
+  ratings.matrix2 <- ratings.matrix[c.rate.rows >= min.gamer.rates, ]
+  # remove games (vars/cols) with fewer than <minRates> games rated
+  rate.cols <- !is.na(ratings.matrix2)[,]
   c.rate.cols <- colSums(rate.cols)
-  ratings.wide3 <- ratings.wide2[, c(TRUE, c.rate.cols >= min.gamer.rates)]
+  ratings.matrix3 <- ratings.matrix2[, c.rate.cols >= min.game.rates]
 
-  ratings.matrix <- as(ratings.wide3[-1], "matrix")
-  rownames(ratings.matrix) <- ratings.wide3$game.id
-  return(ratings.matrix)
+  return(ratings.matrix3)
 }

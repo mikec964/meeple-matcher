5343f0c3c8dd39afbd5c7e661c5fcdc803732b37
Mike Combs <mike@combsnet.com>
2017-12-07 18:48:41 -0500

    New code to compare models. Some vars renamed to make vars easier to browse.

--- recommend.R
+++ recommend.R
@@ -1,5 +1,6 @@
 # recommend.R
 
+rm(list=ls())
 library(dplyr)
 library(readr)
 library(recommenderlab)
@@ -12,7 +13,7 @@ source("scripts/get_game_name.R")
 customer <- "mikec"
 collection.selected <- read_tsv("tables/collection-selected.tsv")
 most.matrix <- MakeRatingMatrix(collection.selected,
-                                min.game.rates=1,
+                                min.game.rates=2,
                                 min.gamer.rates=10)
 print(sprintf("gamers/rows = %s, games/cols = %s",
              nrow(most.matrix), ncol(most.matrix)))
@@ -37,30 +38,63 @@ customer.row <- which(rownames(sample.rrm) == customer)
 #-------------------
 # POPULAR recommender
 #-------------------
-r <- Recommender(sample.rrm, method="POPULAR")
-names(getModel(r))
-getModel(r)$topN
+r.pop <- Recommender(sample.rrm, method="POPULAR")
+# names(getModel(r.pop))
+# getModel(r.pop)$topN
 
-recom <- predict(r, sample.rrm[customer.col], n=5)
-recom.list <- as(recom, "list")[[1]]
-sapply(recom.list, function(x) GetGameName(collection.selected,x))
+r.pop.p <- predict(r.pop, sample.rrm[customer.row], n=5)
+r.pop.l <- as(r.pop.p, "list")[[1]]
+sapply(r.pop.l, function(x) GetGameName(collection.selected,x))
 
 #-------------------
 # SVD recommender
 #-------------------
 # Singular Value Decomposition
 # http://www.cs.carleton.edu/cs_comps/0607/recommend/recommender/svd.html
-r <- Recommender(sample.rrm[1:200], method="SVD")
-names(getModel(r))
-getModel(r)$topN
+r.svd <- Recommender(sample.rrm[1:200], method="SVD")
+# names(getModel(r.svd))
+# getModel(r.svd)$svd
 
-recom <- predict(r, sample.rrm[customer.row], n=5)
-recom.list <- as(recom, "list")[[1]]
-sapply(recom.list, function(x) GetGameName(collection.selected,x))
+r.svd.p <- predict(r.svd, sample.rrm[customer.row], n=5)
+r.svd.l <- as(r.svd.p, "list")[[1]]
+sapply(r.svd.l, function(x) GetGameName(collection.selected,x))
 
 #-------------------
-# Compare POPULAR to SVD
+# UBCF recommender
 #-------------------
-e <- evaluationScheme(sample.rrm[1:200], method="split", train=0.9,
-                      given=2, goodRating=5)
+r.ubcf <- Recommender(sample.rrm, method="UBCF")
+r.ubcf.p <- predict(r.ubcf, sample.rrm[customer.row], n=5)
+r.ubcf.l <- as(r.ubcf.p, "list")[[1]]
+sapply(r.ubcf.l, function(x) GetGameName(collection.selected,x))
 
+#-------------------
+# IBCF recommender
+#-------------------
+r.ibcf <- Recommender(sample.rrm, method="UBCF")
+r.ibcf.p <- predict(r.ibcf, sample.rrm[customer.row], n=5)
+r.ibcf.l <- as(r.ibcf.p, "list")[[1]]
+sapply(r.ibcf.l, function(x) GetGameName(collection.selected,x))
+
+
+#-------------------
+# Compare UBCF to SVD
+#-------------------
+e <- evaluationScheme(sample.rrm, method="split", train=0.9,
+                      given=5, goodRating=5)
+e
+r1 <- Recommender(getData(e, "train"), "UBCF")
+r1
+r2 <- Recommender(getData(e, "train"), "SVD")
+r2
+p1 <- predict(r1, getData(e, "known"), type="ratings")
+p1
+p2 <- predict(r2, getData(e, "known"), type="ratings")
+p2
+error <- rbind(
+  UBCF=calcPredictionAccuracy(p1, getData(e, "unknown")),
+  SVD=calcPredictionAccuracy(p2, getData(e, "unknown")))
+error
+
+#-------------------
+# Compare TopN UBCF to SVD
+#-------------------

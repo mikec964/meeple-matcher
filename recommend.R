53981ec653c495527cbb7b4a315f552f844cee80
Mike Combs <mike@combsnet.com>
2017-12-06 20:24:54 -0500

    Streamline to use new MakeRatingMatrix function.

--- recommend.R
+++ recommend.R
@@ -1,76 +1,66 @@
 # recommend.R
 
 library(dplyr)
+library(readr)
 library(recommenderlab)
-source("scripts/wrangle.R")
+source("scripts/make_rating_matrix.R")
 source("scripts/get_game_name.R")
 
-ReloadData()  # From wrangle.R
-# collection.details <- WidenAttrs(
-#   collection.customer[!is.na(collection.customer$rating),],
-#   games.attrs)
-# collection.details <- WidenTags(collection.details, games.attrs)
-games.unique <- sort(unique(collection.selected$game.id))
+#-------------------
+# Load data
+#-------------------
+customer <- "mikec"
+collection.selected <- read_tsv("tables/collection-selected.tsv")
+most.matrix <- MakeRatingMatrix(collection.selected,
+                                min.game.rates=1,
+                                min.gamer.rates=10)
+print(sprintf("gamers/rows = %s, games/cols = %s",
+             nrow(most.matrix), ncol(most.matrix)))
 
-
-# most.grid (data frame)
-# - rows = unique games known to the gamers (27,881)
-# - cols = gamers that have rated at least 10 games (967)
-most.grid <- MostGrid(collection.selected, 10)
-
-
-# most.matrix
-# row per user, col per game, rating is value
-game.ids <- most.grid$game.id
-most.matrix <- data.matrix(most.grid[-c(1,2)]) # remove game, game.id cols
-rownames(most.matrix) <- game.ids
-dimnames(most.matrix) <- list(item=rownames(most.matrix),
-                              user=colnames(most.matrix))
-most.matrix <- t(most.matrix)
-
-# Find the row with the customer
-customer.row <- which(rownames(most.matrix) == customer)
-
-
+# Select sample (all users, subset of games)
 most.rrm <- as(most.matrix, "realRatingMatrix")
-#most.rrmn <- normalize(r)
-sample.rrm <- sample(most.rrm, 100)
+sample.rrm <- most.rrm
+# sample.rrm <- sample(most.rrm, 10)
+customer.row <- which(rownames(sample.rrm) == customer)
 
-rowCounts(most.rrm[1,])
-as(most.rrm[1,], "list")
-rowMeans(most.rrm[1,])
-hist(getRatings(most.rrm), breaks=10)
-hist(getRatings(normalize(most.rrm, method="Z-score")), breaks=10)
-hist(rowCounts(most.rrm), breaks=50) # how many games have the rated?
-hist(colMeans(most.rrm), breaks=20)  # mean ratings for jokes
+#-------------------
+# Browse data
+#-------------------
+# rowCounts(sample.rrm[1,])
+# as(sample.rrm[1,], "list")
+# rowMeans(sample.rrm[1,])
+# hist(getRatings(sample.rrm), breaks=10)
+# hist(getRatings(normalize(sample.rrm, method="Z-score")), breaks=10)
+# hist(rowCounts(sample.rrm), breaks=50) # how many games have the rated?
+# hist(colMeans(sample.rrm), breaks=20)  # mean ratings for jokes
 
 #-------------------
 # POPULAR recommender
 #-------------------
-r <- Recommender(most.rrm[1:200], method="POPULAR")
+r <- Recommender(sample.rrm, method="POPULAR")
 names(getModel(r))
 getModel(r)$topN
 
-recom <- predict(r, most.rrm[customer.row], n=5)
+recom <- predict(r, sample.rrm[customer.col], n=5)
 recom.list <- as(recom, "list")[[1]]
-sapply(recom.list, function(x) GetGameName(most.grid,x))
+sapply(recom.list, function(x) GetGameName(collection.selected,x))
 
 #-------------------
 # SVD recommender
 #-------------------
 # Singular Value Decomposition
 # http://www.cs.carleton.edu/cs_comps/0607/recommend/recommender/svd.html
-r <- Recommender(most.rrm[1:200], method="SVD")
+r <- Recommender(sample.rrm[1:200], method="SVD")
 names(getModel(r))
 getModel(r)$topN
 
-recom <- predict(r, most.rrm[customer.row], n=5)
+recom <- predict(r, sample.rrm[customer.row], n=5)
 recom.list <- as(recom, "list")[[1]]
-sapply(recom.list, function(x) GetGameName(most.grid,x))
+sapply(recom.list, function(x) GetGameName(collection.selected,x))
 
 #-------------------
 # Compare POPULAR to SVD
 #-------------------
-e <- evaluationScheme(most.rrm[1:200], method="split", train=0.9,
+e <- evaluationScheme(sample.rrm[1:200], method="split", train=0.9,
                       given=2, goodRating=5)
 

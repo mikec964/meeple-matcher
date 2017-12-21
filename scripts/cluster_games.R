3b382abfbefdf2782af928a745eb876404c917d5
Mike Combs <mike@combsnet.com>
2017-12-20 20:46:21 -0500

    plot dendogram for full rating matrix and subset matrix

--- scripts/cluster_games.R
+++ scripts/cluster_games.R
@@ -10,20 +10,28 @@ collection <- collection.selected
 min.rates <- 5
 
 # make ratings DF, row per gamer, col per game
+mean.rating <- round(mean(ratings.tall$rating), 2)
 ratings.tall <- collection %>%
   select(game.id, gamer, rating) %>%            # keep only key attributes
   distinct(gamer, game.id, .keep_all=TRUE) %>%  # remove dupe ratings
   filter(!is.na(rating)) %>%                    # remove non-rated games
   arrange(game.id, gamer)                       # sort by game
-ratings.wide <- spread(ratings.tall, game.id, rating)
+ratings.wide <- spread(ratings.tall, game.id, rating, fill=mean.rating)
+# ratings.wide[is.na(ratings.wide)] <- mean.rating
 
 # make game ratings matrix (row per game), then games distance table
 grm <- as.matrix(ratings.wide[, -1])
 # rownames(grm) <- ratings.wide[, 1]
 # grm <- grm[, -1]
 grm <- t(grm)
-grm.small <- grm[1:9, 1:9]
-games_dist <- dist(grm.small, method="euclidean", diag=F, upper=F)
-games_dist
 
-games_dendro <- hclust(games_dist)
+grms <- grm[1:9, 1:9]
+grms.dist <- dist(grms, method="euclidean", diag=T, upper=F)
+grms.dist
+grs.dendro <- hclust(grms.dist)
+plot(grs.dendro)
+
+grm.dist <- dist(grm, method="euclidean", diag=T, upper=F) # 90sec calc time!
+grm.dist
+grm.dendro <- hclust(grm.dist)
+plot(grm.dendro)

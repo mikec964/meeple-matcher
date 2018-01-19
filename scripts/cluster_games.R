1bd9848c5298980614a1b9383c3f50a6dbce0539
Mike Combs <mike@combsnet.com>
2018-01-19 13:52:46 -0500

    Isolating bug of duplicate game.ids

--- scripts/cluster_games.R
+++ scripts/cluster_games.R
@@ -6,10 +6,16 @@ library(tidyr)
 # test setup
 rm(list = ls())
 source("scripts/make_game_rating_matrix.R")
+source("scripts/wrangle.R")
+
 customer <- "mikec"
 collection <- read_tsv("tables/collection-selected.tsv")
 min_rates <- 5
 ratings <- makeGameRatingMatrix(collection)
+
+## Why aren't game names unique?
+gnames <- rownames(ratings) # 15984 elements
+gnames <- unique(gnames)    # 15770 elements
 
 # work with a subset for speed. There are 15,984 games, 1000 gamers in data.
 grms <- ratings[1:5000, 1:1000]  # 5K games, 1K gamers.
@@ -17,7 +23,30 @@ grms <- ratings[1:5000, 1:1000]  # 5K games, 1K gamers.
 grms_dist <- dist(grms, method="euclidean", diag=T, upper=F)  # long calc time
 
 grs_dendro <- hclust(grms_dist)
-c1 <- cutree(grs_dendro, k=50) # cut into 50 groups
-names(c1)[c1==8]
-c2 <- cut(as.dendrogram(grs_dendro), h=100)
+c1 <- cutree(grs_dendro, k=500) # cut into k groups
+c1df <- data.frame("clust"=c1, "game"=names(c1))
 
+
+# duplicate game names with different clusters??!!
+c1dfu <- c1df %>%
+  distinct(game)
+# names(c1)[c1==8]
+
+# Make gameInfo with game attributes and cluster number
+# We only have attrs for games in my collection (160 of 5000 games)
+gameInfo <- collection %>%
+  select(game.id, game) %>%
+  distinct(game.id, .keep_all=TRUE) %>%
+  arrange(game.id)
+gameInfo2 <- left_join(gameInfo, c1dfu, by="game")
+
+gameAttrs <- read_tsv("tables/games-attrs.tsv")
+gameInfo <- WidenTags(gameInfo, gameAttrs)
+
+gameInfo <- as.data.frame(names(c1))
+myGameIds <- unique(gameAttrs$game.id)
+myCollection <- collection[collection$game.id %in% myGameIds, ]
+
+
+
+# c2 <- cut(as.dendrogram(grs_dendro), h=100)
--- scripts/make_game_rating_matrix.R
+++ scripts/make_game_rating_matrix.R
@@ -3,20 +3,26 @@ library(readr)
 library(tibble)
 library(tidyr)
 
-makeGameRatingMatrix <- function(collection, ratingsPerGame = 1,
-                                 ratingsPerGamer = 5) {
-  # Return matrix of ratings with row per gamer, col per game
-  # ratingsPerGame, ratingsPerGamer ignored at present
+makeGameRatingMatrix <- function(collection, mrpGame = 1, mrpGamer = 5) {
+  # Return matrix of ratings with row per game, col per gamer
+  # mrpGame is min number of ratings of a game to include it
+  # mrpGamer is min number of ratings from a gamer to include them
+  # mrpGame & mrpGamer aren't yet implemented, and
+  # filtering gamers may drop games which in turn drops gamers...
 
-  # make ratings DF, row per gamer, col per game
   ratings_tall <- collection %>%
     select(game.id, game, gamer, rating) %>%      # keep only key attributes
     distinct(gamer, game.id, .keep_all=TRUE) %>%  # remove dupe ratings
     filter(!is.na(rating)) %>%                    # remove non-rated games
     arrange(game.id, gamer)                       # sort by game
 
+  # ratings_wide is row per gamer, col per game.id (sideways to goal)
   ratings_wide <- spread(ratings_tall[, -2], game.id, rating, fill=NA)
   ratings_wide2 <- column_to_rownames(remove_rownames(ratings_wide), "gamer")
+
+  stopifnot(
+    length(colnames(ratings_wide)) == length(unique(colnames(ratings_wide))),
+    length(rownames(ratings_wide)) == length(unique(rownames(ratings_wide))))
 
   # Warning: some games have multiple names in different languages
   unique_names <- ratings_tall %>%
@@ -29,6 +35,10 @@ makeGameRatingMatrix <- function(collection, ratingsPerGame = 1,
   # colnames(ratings_wide2) is the id of the game
   # games[gid] is the name of the game
   colnames(ratings_wide2) <- games[colnames(ratings_wide2)]
+
+  stopifnot(
+    length(colnames(ratings_wide2)) == length(unique(colnames(ratings_wide2))),
+    length(rownames(ratings_wide2)) == length(unique(rownames(ratings_wide2))))
 
   # get mean per player, normalize his scores?
 

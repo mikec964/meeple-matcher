76d82fba13195807ed2e3d895d3e8162d6618774
Mike Combs <mike@combsnet.com>
2018-01-25 15:23:10 -0500

    Fixed problem due to duplicate game names. Now returns gamename.gameid as row labels.

--- scripts/make_game_rating_matrix.R
+++ scripts/make_game_rating_matrix.R
@@ -1,12 +1,11 @@
 library(dplyr)
-library(readr)
 library(tibble)
 library(tidyr)
 
 makeGameRatingMatrix <- function(collection, mrpGame = 1, mrpGamer = 5) {
   # Return matrix of ratings with row per game, col per gamer
-  # mrpGame is min number of ratings of a game to include it
-  # mrpGamer is min number of ratings from a gamer to include them
+  # mrpGame is min number of ratings per game to include it
+  # mrpGamer is min number of ratings per gamer to include them
   # mrpGame & mrpGamer aren't yet implemented, and
   # filtering gamers may drop games which in turn drops gamers...
 
@@ -18,33 +17,36 @@ makeGameRatingMatrix <- function(collection, mrpGame = 1, mrpGamer = 5) {
 
   # ratings_wide is row per gamer, col per game.id (sideways to goal)
   ratings_wide <- spread(ratings_tall[, -2], game.id, rating, fill=NA)
-  ratings_wide2 <- column_to_rownames(remove_rownames(ratings_wide), "gamer")
+  ratings_wide <- column_to_rownames(remove_rownames(ratings_wide), "gamer")
 
-  stopifnot(
+  stopifnot( # are all columns and rows unique?
     length(colnames(ratings_wide)) == length(unique(colnames(ratings_wide))),
     length(rownames(ratings_wide)) == length(unique(rownames(ratings_wide))))
 
-  # Warning: some games have multiple names in different languages
+  # Warning: Some games have multiple names (different languages, publishers),
+  # and game names are not unique
   unique_names <- ratings_tall %>%
     select(game.id, game) %>%
     distinct(game.id, .keep_all=TRUE) %>%
     arrange(game.id)
-  games <- unique_names$game               # value is game name
+  games <- lapply(unique_names$game.id, function(x) {
+    paste0(unique_names[x, "game"], ".", x)
+    })                                     # value is <game name>.<game.id>
   names(games) <- unique_names$game.id     # key is game.id
 
-  # colnames(ratings_wide2) is the id of the game
+  # colnames(ratings_wide) is the id of the game
   # games[gid] is the name of the game
   # add game names as colnames
-  colnames(ratings_wide2) <- games[colnames(ratings_wide2)]
+  colnames(ratings_wide) <- games[colnames(ratings_wide)]
 
-  stopifnot(
-    length(colnames(ratings_wide2)) == length(unique(colnames(ratings_wide2))),
-    length(rownames(ratings_wide2)) == length(unique(rownames(ratings_wide2))))
+  stopifnot( # are all columns and rows unique?
+    length(colnames(ratings_wide)) == length(unique(colnames(ratings_wide))),
+    length(rownames(ratings_wide)) == length(unique(rownames(ratings_wide))))
 
-  ratings <- t(ratings)  # row per game, col per gamer
+  ratings <- t(as.matrix(ratings_wide)) # row per game, col per gamer
 
   # get mean per game, fill into NAs per game
-  ratings <- apply(ratings_wide2, 1, function(x) {  # per row/game
+  ratings <- apply(ratings_wide, 1, function(x) {  # per row/game
     mpc <- mean(x, na.rm=T)             # calc mean
     rt <- ifelse(is.na(x), mpc, x)      # replace na with mean
     return(rt)

f7d86d43a1c5ef47d8441ca38b6c0cd492490fbb
Mike Combs <mike@combsnet.com>
2018-07-10 19:58:45 -0400

    Compare collection sizes (10 quantiles)

--- stats.R
+++ stats.R
@@ -1,27 +1,40 @@
 library(dplyr)
 library(ggplot2)
-library(readr)
 library(tidyr)
 
 source("scripts/restore_data.R")
 # games_ratings is tall and includes customer and neighbors
 
-########## Compare customer ratings to most ratings
+
+# Compare customer ratings to most ratings --------------------------------
 # tidy the data by rating and customer/neighbor
 games_ratings$customer <- games_ratings$gamer == customer
-ratings <- games_ratings %>%
+rating_prop_tbl <- games_ratings %>%
   group_by(rating, customer) %>%
-  summarize(n = sum(rating))
-# change counts to proportion BUT calc proportion of customer,
-# or proportion of neighbor (not-customer)
-n_cust <- sum(ratings[ratings$customer == TRUE, ])
-n_other <- sum(ratings[ratings$customer == FALSE, ])
-ratings$prop <- ratings$customer * ratings$n/n_cust +
-  (!ratings$customer) * ratings$n/n_other
+  summarize(n = sum(rating)) %>%
+  group_by(customer) %>%
+  mutate(prop = n/sum(n)) # proportion of customer or non-customer votes
 
-ggplot(data=ratings, mapping=aes(x=rating, y=prop, fill=customer)) +
+ggplot(data=rating_prop_tbl, mapping=aes(x=rating, y=prop, fill=customer)) +
   ggtitle("Customer Ratings of Collection") +
   scale_x_continuous(breaks=seq(0,10)) +
   geom_col(position="dodge")
 # note: ratings are screwed left
 
+
+# Compare collection sizes ------------------------------------------------
+# put gamers into quintiles, plot collection size for each
+# (158k gamers with 1,385k ratings)
+q5_qty_tbl <- collection_selected %>%
+  # quintiles by #games per gamer
+  count(gamer) %>%
+  arrange(n) %>%
+  mutate(quant = rep(1:5, each=ceiling(length(gamer)/5))) %>%
+  # mean collection size per quintile
+  group_by(quant) %>%
+  summarize(games_mu = as.integer(mean(n, na.rm=TRUE)),
+            games_sd = as.integer(sd(n, na.rm=TRUE)),
+            games_max = max(n, na.rm=TRUE))
+ggplot(data=q5_qty_tbl, mapping=aes(x=quant, y=games_mu)) +
+  ggtitle("Collection Sizes per Neighbor Quintiles") +
+  geom_col()

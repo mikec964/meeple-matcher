8237152da9c35c36330643ae9486ff77ee14317f
Mike Combs <mike@combsnet.com>
2018-07-16 19:56:19 -0400

    Refactor: Renamed table vars

--- stats.R
+++ stats.R
@@ -9,13 +9,13 @@ source("scripts/restore_data.R")
 ########## Compare customer ratings to most ratings
 # tidy the data by rating and customer/neighbor
 games_ratings$customer <- games_ratings$gamer == customer
-ratings <- games_ratings %>%
+rating_prop_tbl <- games_ratings %>%
   group_by(rating, customer) %>%
   summarize(n = sum(rating)) %>%
   group_by(customer) %>%
   mutate(prop = n/sum(n)) # proportion of customer or non-customer votes
 
-ggplot(data=ratings, mapping=aes(x=rating, y=prop, fill=customer)) +
+ggplot(data=rating_prop_tbl, mapping=aes(x=rating, y=prop, fill=customer)) +
   ggtitle("Customer Ratings of Collection") +
   scale_x_continuous(breaks=seq(0,10)) +
   geom_col(position="dodge")
@@ -25,7 +25,7 @@ ggplot(data=ratings, mapping=aes(x=rating, y=prop, fill=customer)) +
 ########## Compare collection sizes
 # put gamers into quintiles, plot collection size for each
 # (158k gamers with 1,385k ratings)
-qty <- collection_selected %>%
+q5_qty_tbl <- collection_selected %>%
   # quintiles by #games per gamer
   count(gamer) %>%
   arrange(n) %>%
@@ -35,7 +35,7 @@ qty <- collection_selected %>%
   summarize(games_mu = as.integer(mean(n, na.rm=TRUE)),
             games_sd = as.integer(sd(n, na.rm=TRUE)),
             games_max = max(n, na.rm=TRUE))
-ggplot(data=qty, mapping=aes(x=g, y=games_mu)) +
+ggplot(data=q5_qty_tbl, mapping=aes(x=g, y=games_mu)) +
   ggtitle("Collection Sizes per Neighbor Quintiles") +
   geom_col()
 
